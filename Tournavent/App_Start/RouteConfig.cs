58c7fdea97862990dde24f9d6930d66b8fef4ae2
Michael <sennhauser.michael@gmail.com>
2020-11-22 14:41:46 +0100

    i weiss nöd wani gmacht han lul

--- Tournavent/App_Start/RouteConfig.cs
+++ Tournavent/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Tournavent
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Process", action = "Index", id = UrlParameter.Optional }
             );
         }
     }

575e04f63c57104c48deb707e41d00415c75e8b7
Michael <sennhauser.michael@gmail.com>
2020-12-03 08:47:34 +0100

    ups authorize isch nowichtig

--- Tournevent/Tournevent/Controllers/HomeController.cs
+++ Tournevent/Tournevent/Controllers/HomeController.cs
@@ -6,18 +6,22 @@ using System.Web.Mvc;
 
 namespace Tournevent.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
         }
+        [Authorize(Roles = "Administrator")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
+        [Authorize(Roles = "Vereinsverwantwortlicher")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";

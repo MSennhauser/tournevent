a5f7dc5ac6a81b349d7148e474bd4b12fafffc54
Michael <sennhauser.michael@gmail.com>
2020-12-28 14:47:11 +0100

    jo blöd hä vergesse en neue branch ufmache... egal i programmier eh elei

---
+++ Tournevent/Tournevent/Controllers/LayoutController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tournevent.Models;
+
+namespace Tournevent.Controllers
+{
+    public class LayoutController : Controller
+    {
+        private readonly Entities db = new Entities();
+        private readonly UserRoleProvider roleProvider = new UserRoleProvider();
+        // GET: Layout
+        [ChildActionOnly]
+        public ActionResult Wettkaempfe()
+        {
+            if (User.IsInRole("Administrator"))
+            {
+                List<SelectListItem> lst = new List<SelectListItem>();
+                List<Wettkampf> wettkampf = (from w in db.Wettkampf select w).ToList();
+                foreach (var tmp in wettkampf)
+                {
+                    lst.Add(new SelectListItem() { Text = tmp.WettkampfName, Value = tmp.Id.ToString() });
+                }
+                return PartialView("_SelectWettkampf", lst);
+            }
+            else
+            {
+                var email = User.Identity.Name;
+                List<SelectListItem> lst = new List<SelectListItem>();
+                Verein verein = (from b in db.Benutzer
+                                 join v in db.Verein on b.VereinId equals v.Index
+                                 where b.Email == email
+                                 select v).Single();
+
+                List<Wettkampf> wettkampf = (from w in db.Wettkampf
+                                             join vw in db.VereineWettkampf on w.Id equals vw.WettkampfId
+                                             where vw.VereinId == verein.Index select w).ToList();
+                foreach (var tmp in wettkampf)
+                {
+                    lst.Add(new SelectListItem() { Text = tmp.WettkampfName, Value = tmp.Id.ToString() });
+                }
+                return PartialView("_SelectWettkampf", lst);
+            }
+        }
+    }
+}
